3375732764e87778729e4b3aaac7a48fa2ada136
Busra Altun <busraltunb@gmail.com>
2021-06-18 22:25:24 +0300

    Put ve Post endpointlerle pratik yapıldı

--- Pratikler/BookStore/BookStore/Controllers/BooksController.cs
+++ Pratikler/BookStore/BookStore/Controllers/BooksController.cs
@@ -40,12 +40,12 @@ namespace BookStore.Controllers
             },
         };
 
-        //[HttpGet]
-        //public List<Book> GetBooks()
-        //{
-        //    var bookList = BookList.OrderBy( x => x.Id).ToList<Book>();
-        //    return bookList;
-        //}
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
 
         [HttpGet("{id}") ]
         public Book GetById(int id)
@@ -54,11 +54,40 @@ namespace BookStore.Controllers
             return book;
         } //doğru olan yaklaşım budur ıd yle getirmek için, routtan getirmek yani
 
-        [HttpGet]
-        public Book Get([FromQuery] string id)
+        //[HttpGet]
+        //public Book Get([FromQuery] string id)
+        //{
+        //    var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //    return book;
+        //}
+
+        // POST
+        [HttpPost]
+        
+        public IActionResult AddBook ([FromBody] Book newBook )
         {
-            var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
-            return book;
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book is not null) // if (book != null ) şeklinde de yazılabilirdi.
+                return BadRequest();
+            BookList.Add(newBook);
+            return Ok();
         }
+
+        [HttpPut ("{id}")]
+
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null) // if (book == null ) şeklinde de yazılabilirdi.
+                return BadRequest();
+
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+            return Ok();
+        }
+
     }
 }
